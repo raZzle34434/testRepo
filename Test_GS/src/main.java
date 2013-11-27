acb38616b438e5fc324a45ed6b28c7c5d805ad9e
Marcus <Marcus@192.168.186.62>
2013-11-27 10:10:23 +0100

    Test Marcus 10:10

--- Test_GS/src/main.java
+++ Test_GS/src/main.java
@@ -3,7 +3,7 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World4");
-		System.out.println("hi");
+		System.out.println("hi Chris");
 	}
 
 }
