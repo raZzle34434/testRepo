a52c8943bb4605b3634b425c8c94815da23d737b
vi-mscherz <scherz.marcus@gmx.de>
2013-11-27 09:02:49 +0100

    Update main.java

--- Test_GS/src/main.java
+++ Test_GS/src/main.java
@@ -1,9 +1,8 @@
-
 public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World2");
+		System.out.println("Hello World3");
 	}
 
 }
